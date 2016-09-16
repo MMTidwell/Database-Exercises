9c511c9f98b2a0da585c73441bc656c42d5bf29a
Mittsy Tidwell <mtidwell506@gmail.com>
2016-09-16 15:16:21 -0500

    first table

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,1 +1,11 @@
 USE codeup_test_db;
+DROP TABLE IF EXISTS albums;
+CREATE TABLE albums(
+	id int UNSIGNED AUTO_INCREMENT NOT NULL,
+	artist VARCHAR(225) NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	release_date YEAR(4) NOT NULL,
+	sales FLOAT(3, 1) NULL, 
+	genre VARCHAR(255) NOT NULL,
+	PRIMARY KEY(id)
+);
